b65ea2d920487deadc1653c172f8156c5c531fb5
vuurvechter <lennoxjiro@gmail.com>
2026-03-16 14:14:10 +0100

    Fox AI done
    you can now choose to make the fox go anywhere if you make the Fox Target teleport too that spot

--- Portrayal/Assets/AI_Scripts/Target change.cs
+++ Portrayal/Assets/AI_Scripts/Target change.cs
@@ -1,7 +1,10 @@
+using KinematicCharacterController.Examples;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Targetchange : MonoBehaviour
 {
+
 
     public GameObject Fox;
     
@@ -13,19 +16,32 @@ public class Targetchange : MonoBehaviour
     
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            Debug.Log("this works!");
-        }
+    
     }
 
 
     public void OnTriggerEnter(UnityEngine.Collider Fox)
     {
+        Vector3 teleport = transform.position;
+        List<int> numbers = new List<int>();
+        
+        numbers.Add(1);
+        numbers.Add(2);
+        numbers.Add(3);
         Debug.Log("Fox has hit the trigger");
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            int number = numbers[i];
+            if (number == 1)
+            {
+                teleport.x = 0;
+                teleport.z = 14;
+            }
+        }
 
-        //make it be able to teleport
+        transform.position = teleport;
         //make a list for all teleports
         //make it easy to change the destinations
     }
+    
 }
