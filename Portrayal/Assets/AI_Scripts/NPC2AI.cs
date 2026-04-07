32350d2205333e50f25a9385ae5b925b6a2fa3f7
vuurvechter <lennoxjiro@gmail.com>
2026-04-07 16:27:34 +0200

    Fixed the AI

---
+++ Portrayal/Assets/AI_Scripts/NPC2AI.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC2AI : MonoBehaviour
+{
+
+    public int AmountOfAreas;
+    public List<Vector3> locations;
+    public int currentLocation = 0;
+    [SerializeField] public float Interval = 15;
+    float timer;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= Interval)
+        {
+            NextMove();
+            timer -= Interval;
+        }
+    }
+
+
+    public void NextMove()
+    {
+        Vector3 teleport = transform.position;
+
+        teleport = locations[currentLocation];
+        currentLocation++;
+        currentLocation = (currentLocation++) % AmountOfAreas;
+
+
+
+
+        transform.position = teleport;
+
+    }
+}
